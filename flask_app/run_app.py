b3347ee3ee9f19c3e6868fe679cde40884dc6c73
kajarosz <karolinaannajarosz@gmail.com>
2022-05-25 14:18:22 +0200

    Import paths fixed

--- flask_app/run_app.py
+++ flask_app/run_app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask_app.main.routes import configure_routes
-from flask_app.main.models import db
+from main.routes import configure_routes
+from main.models import db
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/bookstore'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://oukvzjqvabqlsx:a29a655a62c206124df42ba5b60fb0f01266bec2c7b197f5d528e348633f2756@ec2-3-231-82-226.compute-1.amazonaws.com:5432/d31j0200983ajp'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
